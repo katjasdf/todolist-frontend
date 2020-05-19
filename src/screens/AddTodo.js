5b0fd59fcd21a97ba9f6fb91b36c8c56c306a6e8
katjasdf <katja.hinkkanen@outlook.com>
2020-05-19 10:24:01 +0300

    really last commit

--- src/screens/AddTodo.js
+++ src/screens/AddTodo.js
@@ -78,7 +78,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 20
   },
   row: {
       flexDirection: 'row',
